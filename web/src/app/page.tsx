5288524c339795b9f807d92c932990973ea87e44
jonathanbraaten <jonathan@mjos.no>
2024-10-04 15:14:58 +0200

    remove  unused import

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
 import ResponsiveWrapper from './components/ResponsiveWrapper';
-import { sanityClient } from '@/data/sanityClient/sanityClient';
+
 export default async function Home() {
   return (
     <>
