1bb456043cbc711075cbe281e6a7ee91d27f09f3
jonathanbraaten <jonathan@mjos.no>
2024-10-04 23:38:57 +0200

    move data fetching to page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,13 +1,25 @@
 import PageBlockHandler from '@/components';
 import Footer from './components/Footer';
 import Header from './components/Header';
-
+import { sanityClient } from '@/data/sanityClient/sanityClient';
+import { PAGE_QUERYResult } from '@/data/types/sanity.types';
+import { PAGE_QUERY } from '@/data/queries/fetchPageQuery';
+import { notFound } from 'next/navigation';
+async function getPage(): Promise<PAGE_QUERYResult> {
+  const pageData = await sanityClient.fetch(PAGE_QUERY, { slug: 'hjem' });
+  return pageData;
+}
 export default async function Home() {
+  const page = await getPage();
+  if (!page) {
+    return notFound();
+  }
+
   return (
     <>
       <Header />
       <main>
-        <PageBlockHandler />
+        <PageBlockHandler data={page} />
       </main>
       <Footer />
     </>
--- web/src/components/index.tsx
+++ web/src/components/index.tsx
@@ -1,15 +1,8 @@
-import { PAGE_QUERY } from '@/data/queries/fetchPageQuery';
-import { sanityClient } from '@/data/sanityClient/sanityClient';
 import HeroPageBlockComponent from './HeroPageBlock';
 import { PAGE_QUERYResult } from '@/data/types/sanity.types';
 
-async function getPage(): Promise<PAGE_QUERYResult> {
-  const pageData = await sanityClient.fetch(PAGE_QUERY, { slug: 'hjem' });
-  return pageData;
-}
-
-export default async function PageBlockHandler() {
-  const data = await getPage();
+export default async function PageBlockHandler({ data }: { data: PAGE_QUERYResult }) {
+  /*   const data = await getPage(); */
 
   return (
     <>
